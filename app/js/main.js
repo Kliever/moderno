4750c39f8d15afc931de4d3eeb8d6f3e1631f976
Kliever <mgrenady@gmail.com>
2020-12-26 21:29:00 +0300

    adaptive header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,5 @@
 $(function(){
 
-  var mixer = mixitup('.products__inner-box');
-
   $(".rate-star").rateYo({
     rating: 5,
     starWidth: "12px",
@@ -14,5 +12,15 @@ $(function(){
     slidesToShow: 4,
     slidesToScroll: 4
   });
+
+  $('.menu__btn').on('click', function(){
+    $('.menu__list').slideToggle();
+  });
+
+  $('.header__btn-menu').on('click', function(){
+    $('.header__box').toggleClass('active');
+  });
+
+  var mixer = mixitup('.products__inner-box');
 
 });
