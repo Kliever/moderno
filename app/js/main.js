24f4959518f82849185514195f097bf0a22d6b0a
Kliever <mgrenady@gmail.com>
2021-01-03 18:37:36 +0300

    end project

--- app/js/main.js
+++ app/js/main.js
@@ -30,6 +30,8 @@ $(function(){
     return false;
   });
 
+  $('input[type="file"], select').styler();
+
   var mixer = mixitup('.products__inner-box');
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -27,6 +27,8 @@ gulp.task('css', function () {
   return gulp.src([
     'node_modules/normalize.css/normalize.css',
     'node_modules/slick-carousel/slick/slick.css',
+    'node_modules/jquery-form-styler/dist/jquery.formstyler.css',
+    'node_modules/jquery-form-styler/dist/jquery.formstyler.theme.css',
     // 'node_modules/rateyo/lib/cjs/rateyo.css'
     // 'node_modules/animate.css/animate.css'
     // 'node_modules/magnific-popup/dist/magnific-popup.css'
@@ -54,6 +56,7 @@ gulp.task('js', function(){
   return gulp.src([
     'node_modules/mixitup/dist/mixitup.js',
     'node_modules/slick-carousel/slick/slick.js',
+    'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
     // 'node_modules/rateyo/lib/cjs/rateyo.js'
     // 'node_modules/magnific-popup/dist/jquery.magnific-popup.js' // всплывающее окно, попап картинок (увеличение картинки при клике)
     'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js'  // предпочтительно попап картинок, видео и прочего (увеличение картинки при клике)
